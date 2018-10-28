3d58263f867a632e9462e976af1552fff5549ccc
Muhammad Athar <muhammad.athar@outlook.co.th>
2018-10-28 21:08:25 +0700

    updated

--- app.coffee
+++ app.coffee
@@ -1,5 +1,5 @@
 Canvas = new Layer
-  width: 1440/3 - 200
-  height: 2960/3 - 200
+  width: 1440/2 - 100
+  height: 2960/2 - 100
   x: Align.center()
   y: Align.center()
--- app.js
+++ app.js
@@ -3,8 +3,8 @@
   var Canvas;
 
   Canvas = new Layer({
-    width: 1440 / 3 - 200,
-    height: 2960 / 3 - 200,
+    width: 1440 / 2 - 100,
+    height: 2960 / 2 - 100,
     x: Align.center(),
     y: Align.center()
   });
